86d2d9dfadcd7dafd402af7178645d2bcd7d54c1
cangussukamilly <kamillycangussu23@gmail.com>
2019-11-07 15:06:36 -0300

    Dicionei as rotas index e nova mensagen

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'pagina inicial'
+    return render_template('index.html')
+
+@app.route('/NovaMensagem')
+def NovaMensagem():
+    return render_template('Mensagens.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
